9bcb23501b64420ef8debcc7d0cbb277be954dc7
Aleksey-Belov <asse.com97@gmail.com>
2022-11-08 08:32:44 +0300

    ,Big add

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import _ from "lodash";
 
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
 
+const name = "James";
 
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fname) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting("James");
